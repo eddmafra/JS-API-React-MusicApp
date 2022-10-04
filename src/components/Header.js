547968b92f64526d1713e052704cf04da15e4377
Eddmafra <eduardo_mafra@hotmail.com>
2022-10-04 13:02:09 -0300

    requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaSearch, FaHeart, FaUserCircle } from 'react-icons/fa';
 import { getUser } from '../services/userAPI';
-// import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
   constructor() {
@@ -31,6 +32,20 @@ class Header extends React.Component {
         Isso é um header
         {(loading && <p>Carregando...</p>)}
         {{ name } && <h4 data-testid="header-user-name">{name}</h4>}
+        <nav>
+          <NavLink data-testid="link-to-search" to="/search">
+            <FaSearch />
+            Busca
+          </NavLink>
+          <NavLink data-testid="link-to-favorites" to="/favorites">
+            <FaHeart />
+            Favoritos
+          </NavLink>
+          <NavLink data-testid="link-to-profile" to="/profile">
+            <FaUserCircle />
+            Perfil
+          </NavLink>
+        </nav>
       </header>
     );
   }
