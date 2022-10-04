3a88fd650f5795c65a69936775fe5c186ed24be8
Eddmafra <eduardo_mafra@hotmail.com>
2022-10-04 11:50:30 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,77 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loginUser: '',
+      loading: false,
+      validateBtn: false,
+    };
+  }
+
+  validateText = () => {
+    const { loginUser } = this.state;
+    const textSizeMin = 3;
+    if (loginUser.length < textSizeMin) {
+      this.setState({ validateBtn: false });
+    } else {
+      this.setState({ validateBtn: true });
+    }
+  };
+
+  handleInputChange = (event) => {
+    const { value } = event.target;
+    this.setState({ loginUser: value }, this.validateText);
+  };
+
+  submitBtn = async () => {
+    const { history } = this.props;
+    const { loginUser } = this.state;
+    this.setState({ loading: true });
+    const user = await createUser({ name: loginUser });
+    this.setState({ loading: false });
+    history.push('/search');
+    console.log(user);
+  };
+
   render() {
+    const { loading, validateBtn } = this.state;
     return (
-      <div data-testid="page-login"> Conteúdo de Login </div>
+      <div data-testid="page-login">
+        { loading
+          ? <p>Carregando...</p>
+          : (
+            <form>
+
+              <label htmlFor="username">
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  placeholder="Nome"
+                  id="username"
+                  onChange={ this.handleInputChange }
+                />
+              </label>
+              <button
+                type="submit"
+                data-testid="login-submit-button"
+                id="submitBtn"
+                disabled={ !validateBtn }
+                onClick={ this.submitBtn }
+              >
+                Entrar
+              </button>
+            </form>)}
+      </div>
     );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: propTypes.shape().isRequired,
+};
