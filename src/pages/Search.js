d5ba844cfab8de1dce8e8b1861c917546e2b2796
Eddmafra <eduardo_mafra@hotmail.com>
2022-10-04 13:12:13 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,55 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      validateBtn: false,
+      searchArtist: '',
+    };
+  }
+
+  validateSearch = () => {
+    const { searchArtist } = this.state;
+    const textSizeMin = 2;
+    if (searchArtist.length < textSizeMin) {
+      this.setState({ validateBtn: false });
+    } else {
+      this.setState({ validateBtn: true });
+    }
+  };
+
+  handleInputChange = (event) => {
+    const { value } = event.target;
+    this.setState({ searchArtist: value }, this.validateSearch);
+  };
+
   render() {
+    const { validateBtn } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search"> Conteúdo de Search </div>
+        <div data-testid="page-search">
+          <form>
+            <label htmlFor="searchArtist">
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                name=""
+                id="searchArtist"
+                placeholder="Artista"
+                onChange={ this.handleInputChange }
+              />
+            </label>
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              disabled={ !validateBtn }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </>
     );
   }
